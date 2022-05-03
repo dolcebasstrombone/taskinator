fa6b1c8da1983e0fd111afe2a7186f841620170a
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-03 15:38:10 -0400

    add function to task name and type inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var addTask = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // create list item
   var listItemEl = document.createElement("li");
-  listItemEl.textContent = "This is a new task.";
   listItemEl.className = "task-item";
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  // add content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+  // add list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
