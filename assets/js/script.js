64a5f1308abf7656b1f7f9914e965043504ace87
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-03 15:47:39 -0400

    refactor js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,28 @@ var addTask = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // package data as object
+  var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+  };
+  //send as argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   //create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  // add content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
+    "</h3><span class='task-type'>" +
+    taskDataObj.type +
+    "</span>";
   listItemEl.appendChild(taskInfoEl);
   // add list item to list
   tasksToDoEl.appendChild(listItemEl);
